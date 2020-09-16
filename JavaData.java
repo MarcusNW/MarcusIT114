cbbf561cd40e4354b67db68272c5c699ebe616bc
MarcusNW <70596921+MarcusNW@users.noreply.github.com>
2020-09-16 15:20:50 -0400

    Add files via upload

---
+++ JavaData.java
@@ -0,0 +1,18 @@
+public class JavaData { 
+   
+   public static void main (String[] args) { 
+   
+   String people = "Greeks";
+
+   System.out.println(people); 
+   
+   int century = 100; 
+   
+   System.out.println(century); 
+   
+   double pie = 3.14;
+   
+   System.out.println(pie); 
+   
+   } 
+}
